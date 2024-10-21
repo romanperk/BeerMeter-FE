55d4540362814b0667f2c1d70e91a8f8f0c1b0ad
Roman Perk LAPTOP <romanperk@seznam.cz>
2024-10-21 19:44:04 +0200

    be build fix

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config({ path: '../.env' });
 
 const app = express();
+const frontendPath = path.join(__dirname, '..', 'dist');
 
 // Serve static files from the Vite build (dist folder)
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(frontendPath));
 
 // All other GET requests not handled before will return index.html (SPA fallback)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 // Middleware
