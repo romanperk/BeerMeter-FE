a1d36c2998ec0a21b06d683a3b17980f3f5f5b5a
Roman Perk PC <romanperk@seznam.cz>
2024-10-21 21:58:59 +0200

    fe deployment changed to vercel

--- server/server.js
+++ server/server.js
@@ -8,15 +8,6 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config({ path: '../.env' });
 
 const app = express();
-const frontendPath = path.join(__dirname, '../../dist');
-
-// Serve static files from the Vite build (dist folder)
-app.use(express.static(frontendPath));
-
-// All other GET requests not handled before will return index.html (SPA fallback)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
 
 // Middleware
 app.use(bodyParser.json());
